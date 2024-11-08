2ab820ebed0633a5a3c6eb0f76efa307cb3d0880
Shourya wadhwa <71631493+CoderLogy@users.noreply.github.com>
2024-11-07 20:23:59 -0800

    Biggest Update
    added support for firefox and chrome.
    Be happy and run both

--- chrome/background.js
+++ chrome/background.js
@@ -5,7 +5,8 @@ function updateTabCount() {
     chrome.tabs.query({}, function (tabs) {
         const tabCount = tabs.length;
         chrome.action.setBadgeText({ text: tabCount.toString() });
-        chrome.action.setBadgeBackgroundColor({ color: [0, 0, 0, 255] });
+        chrome.action.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
+        chrome.action.setBadgeTextColor({ color: 'white' });
     });
 }
 
